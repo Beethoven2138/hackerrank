cb68cf8be3f232de5eefd9872be9a6a435f17ff4
Saxon <saxon.supple@gmail.com>
2018-10-20 15:57:16 +0100

    Update matrixlayerrotation.c

--- matrixlayerrotation.c
+++ matrixlayerrotation.c
@@ -4,8 +4,6 @@ void tmp_ring(char even, int *tmp, int **matrix, int matrix_rows, int matrix_col
     unsigned int startx, starty;
     if (even)
     {
-        //startx = (matrix_rows - hor) / 2;
-        //starty = (matrix_columns - vert) / 2;
         startx = (matrix_columns - hor) / 2;
         starty = (matrix_rows - vert) / 2;
     }
@@ -17,11 +15,11 @@ void tmp_ring(char even, int *tmp, int **matrix, int matrix_rows, int matrix_col
     int off = 0;
     for (int i = 0; i < vert-1; ++i)
     {
-        tmp[1+/*i*/off++] = matrix[starty + i][startx];
+        tmp[1+off++] = matrix[starty + i][startx];
     }
     for (int i = 0; i < hor-1; ++i)
     {
-        tmp[1+/*i+vert*/off++] = matrix[starty + vert-1][startx + i];
+        tmp[1+off++] = matrix[starty + vert-1][startx + i];
     }
     for (int i = vert-1; i > 0; --i)
     {
@@ -39,8 +37,6 @@ void reverse_tmp_ring(char even, int *tmp, int **matrix, int matrix_rows, int ma
     unsigned int startx, starty;
     if (even)
     {
-        //startx = (matrix_rows - hor) / 2;
-        //starty = (matrix_columns - vert) / 2;
         startx = (matrix_columns - hor) / 2;
         starty = (matrix_rows - vert) / 2;
     }
@@ -68,7 +64,18 @@ void reverse_tmp_ring(char even, int *tmp, int **matrix, int matrix_rows, int ma
     }
 }
 
-void shift(int *ring, int len)
+/*void shift(int *ring, int len, int times)
+{
+    /*if (times == 0)
+        return;
+    int tmp[times]/* = ring[len-times]*/;
+    //memcpy(tmp, (void*)(ring+len-times), times * sizeof(int));
+    //memmove((void*)(ring+times), (void*)ring, (len-times) * sizeof(int));
+    //ring[times-1] = tmp;
+    //memcpy(ring, tmp, times * sizeof(int));*/
+//}*/
+
+void shift(int *ring, int len, int times)
 {
     int tmp = ring[len-1];
     memmove((void*)(ring+1), (void*)ring, (len-1) * sizeof(int));
@@ -84,32 +91,16 @@ void matrixRotation(int matrix_rows, int matrix_columns, int** matrix, int r)
         int top_center = (matrix_rows/2)-1;
         int bottom_center = top_center + 1;
         int smallest_loop_len = matrix_columns - top_center * 2;
-        int *rings[matrix_rows/2];
         for (int gap = 0; gap <= matrix_rows - 2; gap += 2)
         {
             int vert = gap + 2;
             int hor = smallest_loop_len + gap;
             int tmp_len = (gap+1)*2 + (smallest_loop_len + gap-1)*2;
-            //int *tmp = (int*)malloc(sizeof(int) * tmp_len);
-            rings[gap/2] = (int*)malloc(sizeof(int) * tmp_len);
-            tmp_ring(1, rings[gap/2], matrix, matrix_rows, matrix_columns, vert, hor);
-            //reverse_tmp_ring(1, tmp, matrix, matrix_rows, matrix_columns, vert, hor);
-            //free(tmp);
-        }
-        while (r-- > 0)
-        {
-            for (int gap = 0; gap <= matrix_rows - 2; gap += 2)
-            {
-                int vert = gap + 2;
-                int hor = smallest_loop_len + gap;
-                int tmp_len = (gap+1)*2 + (smallest_loop_len + gap-1)*2;
-                reverse_tmp_ring(1, rings[gap/2], matrix, matrix_rows, matrix_columns, vert, hor);
-                shift(rings[gap/2], tmp_len);
-            }
-        }
-        for (int gap = 0; gap <= matrix_rows - 2; gap += 2)
-        {
-            free(rings[gap/2]);
+            int tmp[tmp_len];
+            tmp_ring(1, tmp, matrix, matrix_rows, matrix_columns, vert, hor);
+            for (int z = 0; z < r-1; ++z)
+                shift(tmp, tmp_len, r-1);
+            reverse_tmp_ring(1, tmp, matrix, matrix_rows, matrix_columns, vert, hor);
         }
     }
     else
@@ -123,26 +114,11 @@ void matrixRotation(int matrix_rows, int matrix_columns, int** matrix, int r)
             int hor = gap + 2;
             int vert = smallest_loop_len + gap;
             int tmp_len = (gap+1)*2 + (smallest_loop_len + gap-1)*2;
-            //int *tmp = (int*)malloc(sizeof(int) * tmp_len);
-            rings[gap/2] = (int*)malloc(sizeof(int) * tmp_len);
-            tmp_ring(0, rings[gap/2], matrix, matrix_rows, matrix_columns, vert, hor);
-            //reverse_tmp_ring(0, tmp, matrix, matrix_rows, matrix_columns, vert, hor);
-            //free(tmp);
-        }
-        while (r-- > 0)
-        {
-            for (int gap = 0; gap <= matrix_columns - 2; gap += 2)
-            {
-                int hor = gap + 2;
-                int vert = smallest_loop_len + gap;
-                int tmp_len = (gap+1)*2 + (smallest_loop_len + gap-1)*2;
-                reverse_tmp_ring(0, rings[gap/2], matrix, matrix_rows, matrix_columns, vert, hor);
-                shift(rings[gap/2], tmp_len);
-            }    
-        }
-        for (int gap = 0; gap <= matrix_columns - 2; gap += 2)
-        {
-            free(rings[gap/2]);
+            int tmp[tmp_len];
+            tmp_ring(0, tmp, matrix, matrix_rows, matrix_columns, vert, hor);
+            for (int z = 0; z < r-1; ++z)
+                shift(tmp, tmp_len, r-1);
+            reverse_tmp_ring(0, tmp, matrix, matrix_rows, matrix_columns, vert, hor);
         }
     }
 
